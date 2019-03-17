8867aa018f4062d7c2b93c6ea0b15acf6ea53453
Abdel Mejia <bhalut.bear@gmail.com>
2019-03-17 17:15:19 -0500

    Add KillZone script

---
+++ SpaceMan/Assets/SpaceMan/Scripts/Util/KillZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            player.Die();
+        }
+    }
+}
