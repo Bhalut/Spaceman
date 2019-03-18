b8dd533ededcae8f0c8c306b09edf1c16b0b1d49
Abdel Mejia <bhalut.bear@gmail.com>
2019-03-17 23:28:21 -0500

    Add LevelManager script

---
+++ SpaceMan/Assets/SpaceMan/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    public List<LevelBlock> allLevelsBlock = new List<LevelBlock>();
+    public List<LevelBlock> currentLevelBlock = new List<LevelBlock>();
+    public Transform levelStartPosition;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        GenerateInitBlock();
+    }
+
+    public void AddLevelBlock()
+    {
+        int randomIdx = Random.Range(0, allLevelsBlock.Count);
+        LevelBlock block;
+        Vector2 spawnPosition = Vector2.zero;
+        if (currentLevelBlock.Count == 0)
+        {
+            block = Instantiate(allLevelsBlock[0]);
+            spawnPosition = levelStartPosition.position;
+        }
+        else
+        {
+            block = Instantiate(allLevelsBlock[randomIdx]);
+            spawnPosition = currentLevelBlock[currentLevelBlock.Count - 1].exitPoint.position;
+        }
+
+        block.transform.SetParent(this.transform, false);
+
+        Vector2 correction = new Vector2(spawnPosition.x - block.startPoint.position.x, spawnPosition.y - block.startPoint.position.y);
+        block.transform.position = correction;
+        currentLevelBlock.Add(block);
+    }
+
+    public void RemoveLevelBlock()
+    {
+
+    }
+
+    public void RemoveAllLevelBlock()
+    {
+
+    }
+
+    public void GenerateInitBlock()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            AddLevelBlock();
+        }
+    }
+}
