27896b73755c4666a48c39952c3d79daedcc2cfa
Abdel Mejia <bhalut.bear@gmail.com>
2019-03-19 23:27:01 -0500

    Add UiManager script

---
+++ SpaceMan/Assets/SpaceMan/Scripts/Manager/UiManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager instance;
+    public Canvas canvas;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ShowMainMenu()
+    {
+        canvas.enabled = true;
+    }
+
+    public void HideMainMenu()
+    {
+        canvas.enabled = false;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
