ba62d6e4c07f8e9a2807c007ec581ceb7ebd6c6d
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-19 19:55:59 -0500

    Update GameManager.cs

--- SpaceMan/Assets/SpaceMan/Scripts/Manager/GameManager.cs
+++ SpaceMan/Assets/SpaceMan/Scripts/Manager/GameManager.cs
@@ -6,6 +6,7 @@ public sealed class GameManager : MonoBehaviour
 
     public GameState state;
     private PlayerController player;
+    public int collectedObject = 0;
 
     private void Awake()
     {
@@ -20,17 +21,6 @@ public sealed class GameManager : MonoBehaviour
     private void Start()
     {
         Init();
-    }
-
-    private void Update()
-    {
-        if (state != GameState.InGame)
-        {
-            if (Input.GetKey(KeyCode.S))
-            {
-                StartGame();
-            }
-        }
     }
 
     private void Init()
@@ -58,19 +48,33 @@ public sealed class GameManager : MonoBehaviour
         switch (newGameState)
         {
             case GameState.Menu:
+                UiManager.instance.ShowMainMenu();
+                UiManager.instance.HideGameMenu();
+                UiManager.instance.HideGameOverMenu();
                 break;
             case GameState.InGame:
                 LevelManager.instance.RemoveAllLevelBlock();
                 LevelManager.instance.GenerateInitBlock();
+                UiManager.instance.HideMainMenu();
+                UiManager.instance.ShowGameMenu();
+                UiManager.instance.HideGameOverMenu();
                 player.Init();
                 break;
             case GameState.GameOver:
+                UiManager.instance.HideMainMenu();
+                UiManager.instance.HideGameMenu();
+                UiManager.instance.ShowGameOverMenu();
                 break;
             default:
                 goto case GameState.Menu;
         }
 
         this.state = newGameState;
+    }
+
+    public void CollectObject(Collectable collectable)
+    {
+        collectedObject += collectable.value;
     }
 }
 
