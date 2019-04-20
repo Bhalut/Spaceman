1b5c443d1dc54f98ee88b55f7723d3d511735b87
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-19 19:55:55 -0500

    Update LevelManager.cs

--- SpaceMan/Assets/SpaceMan/Scripts/Manager/LevelManager.cs
+++ SpaceMan/Assets/SpaceMan/Scripts/Manager/LevelManager.cs
@@ -62,7 +62,7 @@ public class LevelManager : MonoBehaviour
 
     public void GenerateInitBlock()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 4; i++)
         {
             AddLevelBlock();
         }
