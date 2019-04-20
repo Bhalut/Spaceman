18c2aed97c6314f76c1638ab39372b52ad07c561
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-19 19:52:48 -0500

    Create ExitZone script

---
+++ SpaceMan/Assets/SpaceMan/Scripts/Util/ExitZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ExitZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LevelManager.instance.AddLevelBlock();
+            LevelManager.instance.RemoveLevelBlock();
+        }
+    }
+}
