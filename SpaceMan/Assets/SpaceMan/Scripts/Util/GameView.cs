a522b470e969cd4fd054a5f215453cdc1cd4cdc1
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-19 19:53:23 -0500

    Create GameView script

---
+++ SpaceMan/Assets/SpaceMan/Scripts/Util/GameView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameView : MonoBehaviour
+{
+    public Text coinsText, scoreText, maxScoreText;
+
+    private PlayerController player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (GameManager.instance.state == GameState.InGame)
+        {
+            int coins = GameManager.instance.collectedObject;
+            float score = player.GetTravelledDistance();
+            float maxScore = PlayerPrefs.GetFloat("maxscore", 0);
+
+            coinsText.text = coins.ToString();
+            scoreText.text = "Score: " + score.ToString("f1");
+            maxScoreText.text = "MaxScore: " + maxScore.ToString("f1");
+        }
+    }
+}
