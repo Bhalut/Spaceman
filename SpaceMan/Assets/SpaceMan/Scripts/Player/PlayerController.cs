2bc69e41a6ab957ab142727c04927c4047b515a5
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-19 19:56:15 -0500

    Update PlayerController.cs

--- SpaceMan/Assets/SpaceMan/Scripts/Player/PlayerController.cs
+++ SpaceMan/Assets/SpaceMan/Scripts/Player/PlayerController.cs
@@ -2,35 +2,44 @@
 
 public class PlayerController : MonoBehaviour
 {
-    private float jumpForce = 6f;
+    private float jumpForce = 10f;
     private float speed = 2f;
-    private bool isJump;
-    private Rigidbody2D rb;
+    private bool _isJump;
+    private Rigidbody2D _rb;
     public LayerMask groundMask;
-    private Animator anim;
-    private Vector2 startPosition;
-    private const string STATE_ALIVE = "isAlive";
-    private const string STATE_GROUND = "isGround";
-    private const string STATE_DIE = "isDie";
+    private Animator _anim;
+    private Vector2 _startPosition;
+    private const string StateAlive = "isAlive";
+    private const string StateGround = "isGround";
+    private const string StateDie = "isDie";
+
+    private int _healthPoints, _manaPoints;
+    private static readonly int IsAlive = Animator.StringToHash(StateAlive);
+    private static readonly int IsGround = Animator.StringToHash(StateGround);
+
+    public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15,
+        MAX_HEALTH = 200, MAX_MANA = 30,
+        MIN_HEALTH = 10, MIN_MANA = 0;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Start()
     {
-        startPosition = this.transform.position;
+        _startPosition = this.transform.position;
         Init();
     }
 
     private void Update()
     {
-        isJump = Input.GetKeyDown(KeyCode.Space);
-
-
-        State();
+        #if UNITY_ANDROID || UNITY_IOS
+        _isJump = Input.GetButtonDown("Fire1");
+        #else
+        _isJump = Input.GetButtonDown("Fire1");
+        #endif
 
         Debug.DrawRay(transform.position, Vector2.down * 1.45f, Color.gray);
     }
@@ -39,42 +48,42 @@ public class PlayerController : MonoBehaviour
     {
         if (GameManager.instance.state == GameState.InGame)
         {
-            if (rb.velocity.x < speed)
+            if (_rb.velocity.x < speed)
             {
-                rb.velocity = new Vector2(speed, rb.velocity.y);
+                _rb.velocity = new Vector2(speed, _rb.velocity.y);
             }
 
-            if (isJump)
+            if (_isJump)
             {
                 Jump();
             }
         }
         else
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
         }
 
-    }
-
-    private void State()
-    {
-        anim.SetBool(STATE_GROUND, IsTouchingOnTheGround());
     }
 
     private void Jump()
     {
         if (IsTouchingOnTheGround())
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            GetComponent<AudioSource>().Play();
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _anim.SetBool(IsGround, IsTouchingOnTheGround());
         }
     }
 
     public void Init()
     {
-        anim.SetBool(STATE_ALIVE, true);
-        anim.SetBool(STATE_GROUND, false);
+        _anim.SetBool(IsAlive, true);
+        _anim.SetBool(IsGround, false);
 
-        Invoke("RestartPosition", 0.1f);
+        _healthPoints = INITIAL_HEALTH;
+        _manaPoints = INITIAL_MANA;
+
+        Invoke(nameof(RestartPosition), 0.1f);
 
         GameObject cam = GameObject.FindWithTag("MainCamera");
         cam.GetComponent<CameraController>().ResetCameraPosition();
@@ -82,16 +91,64 @@ public class PlayerController : MonoBehaviour
 
     private void RestartPosition()
     {
-        this.transform.position = startPosition;
-        this.rb.velocity = Vector2.zero;
+        this.transform.position = _startPosition;
+        this._rb.velocity = Vector2.zero;
+
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        mainCamera.GetComponent<CameraController>().ResetCameraPosition();
     }
 
     private bool IsTouchingOnTheGround() => Physics2D.Raycast(transform.position, Vector2.down, 1.45f, groundMask);
 
     public void Die()
     {
-        anim.SetTrigger(STATE_DIE);
-        anim.SetBool(STATE_ALIVE, false);
+        float travelledDistance = GetTravelledDistance();
+        float previousMaxDistance = PlayerPrefs.GetFloat("maxscore", 0f);
+        if (travelledDistance > previousMaxDistance)
+        {
+            PlayerPrefs.SetFloat("maxscore", travelledDistance);
+        }
+
+        this._anim.SetBool(IsAlive, false);
+        this._anim.SetTrigger(StateDie);
         GameManager.instance.GameOver();
+    }
+
+    public void CollectHealth(int points)
+    {
+        this._healthPoints += points;
+        if (this._healthPoints >= MAX_HEALTH)
+        {
+            this._healthPoints = MAX_HEALTH;
+        }
+
+        if (this._healthPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void CollectMana(int points)
+    {
+        this._manaPoints += points;
+        if (this._manaPoints >= MAX_MANA)
+        {
+            this._manaPoints = MAX_MANA;
+        }
+    }
+
+    public int GetHealth()
+    {
+        return _healthPoints;
+    }
+
+    public int GetMana()
+    {
+        return _manaPoints;
+    }
+
+    public float GetTravelledDistance()
+    {
+        return this.transform.position.x - _startPosition.x;
     }
 }
